fba5f98c0490bf5d9b8758e2619586b66adb297a
Divyarani R <69153085+Divyarani30@users.noreply.github.com>
2022-04-22 13:18:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const user = require("./routes/users");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log("connected to mongoDB")
+})
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+
+
+app.get("/", (req, res) => {
+    res.send("welcome to insta")
+})
+
+app.use("/api/user", user);
+
+app.listen(3000, () => {
+    console.log("Backend server is running on 3000")
+})
+
---
+++ routes/users.js
@@ -0,0 +1,47 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+
+router.post("/signup", async (req, res) => {
+    console.log("outside coming inside", req.body);
+    try {
+        console.log("coming inside", req.body);
+        const { username, email, password } = req.body;
+        console.log("coming inside", username, email, password);
+        //encrypt new password
+        const salt = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, salt);
+
+        //create new user
+        const newUser = new User({
+            username, email, password: hashPassword
+        });
+
+        const user = await newUser.save();
+        res.status(200).json({
+            message: 'registered successfully',
+            userLoginDetails: user
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'facing issue while register, please try again'
+        })
+        console.log("facing issue while register", error)
+    }
+
+});
+
+// router.post("/login", async (req, res) => {
+//     try {
+//         const { email, password } = req.body;
+//         const isExistingUser = await User.findOne({ email });
+//         !isExistingUser && res.status(404).json("user not found, please sign up");
+
+//     } catch (error) {
+
+//     }
+
+// })
+
+module.exports = router;
